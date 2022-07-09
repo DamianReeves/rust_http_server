1c000bc4a5c9b20a03865987aec8df7b4e4a407a
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-07-09 17:54:27 -0400

    Lifetimes oh my

--- src/http/request.rs
+++ src/http/request.rs
@@ -8,21 +8,19 @@ use std::{
 };
 
 #[derive(Debug)]
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl Request {}
-
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        let request = str::from_utf8(value)?;
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
+        let request = str::from_utf8(buf)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
@@ -35,13 +33,13 @@ impl TryFrom<&[u8]> for Request {
 
         let mut query_string = None;
         if let Some(i) = path.find('?') {
-            query_string = Some(path[i + 1..].to_string());
+            query_string = Some(&path[i + 1..]);
             path = &path[..i];
         }
 
         Ok(Self {
             method,
-            path: path.to_string(),
+            path: path,
             query_string,
         })
     }
