d969064209644594dfe073cd4d87ee9d40aa8c62
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-07-09 21:14:11 -0400

    Serve file

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -3,13 +3,28 @@ use crate::{
     server::Handler,
 };
 
-pub struct WebsiteHandler;
+use std::fs;
+
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        WebsiteHandler { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/user" => Response::new(StatusCode::Ok, Some("<h1>User</h1>".to_string())),
                 _ => Response::new(StatusCode::NotFound, None),
             },
